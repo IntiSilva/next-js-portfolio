51071101235586df5fd837c0fba0640aa30abcfe
Inti Silva <inti.t.silva@gmail.com>
2023-06-17 19:49:22 -0300

    Third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -251,11 +251,7 @@ const Navbar = () => {
                     <FaGithub />
                   </div>
                 </a>
-                <a
-                  href="https://mail.google.com/mail/?view=cm&source=mailto&to=inti.t.silva@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
+                <a href="mailto:inti.t.silva@gmail.com?">
                     <div className="rounded-full bg-[#333333] shadow-lg shadow-[#001011] text-emerald-400 p-3 cursor-pointer focus:scale-105 ease-in duration-300">
                       <SiGmail />
                     </div>
